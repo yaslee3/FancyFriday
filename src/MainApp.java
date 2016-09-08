1455a7fe8c43f1bc2aaf26c46a5ae560db8a9683
Yasmine Lee <yaslee3@yahoo.com>
2016-09-08 11:43:56 -0400

    initial commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,8 +1,10 @@
-/**
- * 
- */
+import java.util.Scanner;
 
-/**
+
+
+
+ 
+/**this is a demo app on what to wear 
  * @author yas lee
  *
  */
@@ -12,7 +14,29 @@ public class MainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String inputFancyFriday,inputSunny, inputRaining;
+		boolean fancyfriday,sunny,raining;
+		
+		// Get input from user
+		Scanner input = new Scanner(System.in);
+		System.out.println("Is it fancy friday?");
+		inputFancyFriday = input.nextLine();
+		
+		System.out.println("Is it sunny?");
+		inputSunny = input.nextLine();
+		
+		System.out.println("Is it raining?");
+		inputRaining = input.nextLine();
+		
+		//test if the variable are working properly
+		//System.out.println(inputFancyFriday + "" + inputSunny);
+		
+		//Set condition variables base on input
+		
+		// use if statement to decide what to wear
+		
+		
 
 	}
 
